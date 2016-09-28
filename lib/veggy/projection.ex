3438b07c0997c014b4c1fa84515fff8eefb8aaea
gabriele.lana <gabriele.lana@cleancode.it>
2016-09-28 12:56:44 +0200

    Update Veggy.Projection behaviour

--- lib/veggy/projection.ex
+++ lib/veggy/projection.ex
@@ -1,6 +1,13 @@
 defmodule Veggy.Projection do
   use GenServer
 
+  # defcallback init() :: {events :: [String.t], offset :: integer}
+  # defcallback fetch(event :: Map.t) :: record :: Map.t
+  # defcallback store(record :: Map.t, offset :: integer) :: :ok | {:error, reason :: String.t}
+  # defcallback delete(record :: Map.t) :: :ok | {:error, reason :: String.t}
+  # defcallback process(event :: Map.t, record :: Map.t) :: record :: Map.t | :skip | :hold | :delete | {:error, reason :: String.t}
+  # defcallback query(report_name :: String.t, query :: Map.t) :: report :: [record :: Map.t]
+
   def start_link(module) do
     GenServer.start_link(__MODULE__, %{module: module})
   end
