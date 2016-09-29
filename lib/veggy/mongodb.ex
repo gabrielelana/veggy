abf9ded3105d5a19ce71d5c194d58231143fda29
gabriele.lana <gabriele.lana@cleancode.it>
2016-09-29 15:58:49 +0200

    Extract `Veggy.MongoDB.Aggregate`

--- lib/veggy/mongodb.ex
+++ lib/veggy/mongodb.ex
@@ -31,41 +31,12 @@ defmodule Veggy.MongoDB do
     end
   end
 
-  defmodule Aggregate do
-    defmacro __using__(opts) do
-      collection = Keyword.get(opts, :collection);
-
-      quote do
-        if unquote(collection) do
-          @collection unquote(collection)
-        else
-          @collection Veggy.MongoDB.Aggregate.collection_name(__MODULE__)
-        end
-
-        def fetch(id, initial) do
-          case Mongo.find(Veggy.MongoDB, @collection, %{"_id" => id}) |> Enum.to_list do
-            [d] -> d |> Map.put("id", d["_id"]) |> Map.delete("_id")
-            _ -> initial
-          end
-        end
-
-        def store(aggregate) do
-          aggregate = aggregate |> Map.put("_id", aggregate["id"]) |> Map.delete("id")
-          {:ok, _} = Mongo.save_one(Veggy.MongoDB, @collection, aggregate)
-        end
-
-        defoverridable [fetch: 2, store: 1]
-      end
-    end
-
-    def collection_name(module_name) do
-      module_name
-      |> Module.split
-      |> List.last
-      |> String.downcase
-      |> Inflex.pluralize
-      |> (&"aggregate.#{&1}").()
-    end
+  def collection_name(module_name) do
+    module_name
+    |> Module.split
+    |> List.last
+    |> String.downcase
+    |> Inflex.pluralize
   end
 
   def create_index(collection_name, index_keys) do
